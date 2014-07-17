f37217bc79fcb01727218771c6320491880c4769
kwaghray <kunalwaghray52@gmail.com>
2014-07-17 18:33:18 +0530

    firts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Automob::Application.config.secret_token = '219d0d5303db179d871ef23a2a7dd700453025b4693672bdd436dae11fad2fbbb160563120464cd45062ab9f8bb7e63693990cc1ca67043d75fe2c9edf938c12'
